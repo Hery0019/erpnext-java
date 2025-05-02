2d45e8974fca76e42b7e6ee8b756b51a1d5963cb
Hery <153767421+Hery0019@users.noreply.github.com>
2025-05-02 12:25:13 +0300

    Create ItemDevis.java

---
+++ src/main/java/hery/itu/erp/model/ItemDevis.java
@@ -0,0 +1,18 @@
+package hery.itu.erp.model;
+
+public class ItemDevis {
+    private String code;
+    private String description;
+    private Double prix;
+
+    public String getCode() { return code; }
+    public void setCode(String code) { this.code = code; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public Double getPrix() { return prix; }
+    public void setPrix(Double prix) { this.prix = prix; }
+
+}
+
