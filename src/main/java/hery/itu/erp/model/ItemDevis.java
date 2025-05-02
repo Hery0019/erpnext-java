fa641f03e624281a7e74efaae04ac34e0f6f1911
Hery <153767421+Hery0019@users.noreply.github.com>
2025-05-02 21:14:53 +0300

    Update ItemDevis.java

--- src/main/java/hery/itu/erp/model/ItemDevis.java
+++ src/main/java/hery/itu/erp/model/ItemDevis.java
@@ -3,16 +3,37 @@ package hery.itu.erp.model;
 public class ItemDevis {
     private String code;
     private String description;
-    private Double prix;
+    private double quantite;
+    private String unite;
+    private String numeroDevis;
+    private double prixUnitaire;
+    private double montant;
+    private String entrepot; // warehouse
 
+    // Getters et Setters
     public String getCode() { return code; }
     public void setCode(String code) { this.code = code; }
+
+    public String getNumeroDevis() { return numeroDevis; }
+    public void setNumeroDevis(String numeroDevis) { this.numeroDevis = numeroDevis; }
 
     public String getDescription() { return description; }
     public void setDescription(String description) { this.description = description; }
 
-    public Double getPrix() { return prix; }
-    public void setPrix(Double prix) { this.prix = prix; }
+    public double getQuantite() { return quantite; }
+    public void setQuantite(double quantite) { this.quantite = quantite; }
 
+    public String getUnite() { return unite; }
+    public void setUnite(String unite) { this.unite = unite; }
+
+    public double getPrixUnitaire() { return prixUnitaire; }
+    public void setPrixUnitaire(double prixUnitaire) { this.prixUnitaire = prixUnitaire; }
+
+    public double getMontant() { return montant; }
+    public void setMontant(double montant) { this.montant = montant; }
+
+    public String getEntrepot() { return entrepot; }
+    public void setEntrepot(String entrepot) { this.entrepot = entrepot; }
 }
+
 
