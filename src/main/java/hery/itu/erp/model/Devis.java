b9c9c13686409cb3404033e2eaf6537448d6ebb8
Hery <153767421+Hery0019@users.noreply.github.com>
2025-05-02 21:14:49 +0300

    Update Devis.java

--- src/main/java/hery/itu/erp/model/Devis.java
+++ src/main/java/hery/itu/erp/model/Devis.java
@@ -1,11 +1,14 @@
 package hery.itu.erp.model;
 
+import java.util.List;
+
 public class Devis {
     private String numero;
     private String date;
-    private Double montant;
     private String status;
     private String currency;
+    private Double montant;
+    private List<ItemDevis> items;
 
     // Getters et Setters
     public String getNumero() { return numero; }
@@ -14,12 +17,16 @@ public class Devis {
     public String getDate() { return date; }
     public void setDate(String date) { this.date = date; }
 
-    public Double getMontant() { return montant; }
-    public void setMontant(Double montant) { this.montant = montant; }
-
     public String getStatus() { return status; }
     public void setStatus(String status) { this.status = status; }
 
     public String getCurrency() { return currency; }
     public void setCurrency(String currency) { this.currency = currency; }
+
+    public Double getMontant() { return montant; }
+    public void setMontant(Double montant) { this.montant = montant; }
+
+    public List<ItemDevis> getItems() { return items; }
+    public void setItems(List<ItemDevis> items) { this.items = items; }
 }
+
