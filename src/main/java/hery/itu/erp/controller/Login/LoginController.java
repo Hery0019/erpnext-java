4065f36d59ddceece750e076bc1bd427e088cf09
Hery <153767421+Hery0019@users.noreply.github.com>
2025-05-02 12:21:47 +0300

    Update LoginController.java

--- src/main/java/hery/itu/erp/controller/Login/LoginController.java
+++ src/main/java/hery/itu/erp/controller/Login/LoginController.java
@@ -5,7 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import hery.itu.erp.service.Login.LoginService;
+
+import hery.itu.erp.service.login.LoginService;
 
 
 @Controller
