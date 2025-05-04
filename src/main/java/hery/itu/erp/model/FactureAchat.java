9b555301e8bdddf27d4e1f9d452771cee9a0258e
Hery <153767421+Hery0019@users.noreply.github.com>
2025-05-05 00:01:02 +0300

    Update FactureAchat.java

--- src/main/java/hery/itu/erp/model/FactureAchat.java
+++ src/main/java/hery/itu/erp/model/FactureAchat.java
@@ -4,16 +4,29 @@ package hery.itu.erp.model;
 public class FactureAchat {
     private String name;
     private String supplier;
+    private String supplierName;
     private String postingDate;
     private String status;
     private double grandTotal;
+    private double outstandingAmount;
 
-    // Getters et Setters
+    public FactureAchat() {}
+    public FactureAchat(String name, String supplierName, String postingDate, String status, double outstandingAmount) {
+        this.name = name;
+        this.supplierName = supplierName;
+        this.postingDate = postingDate;
+        this.status = status;
+        this.outstandingAmount = outstandingAmount;
+    }
+
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
 
     public String getSupplier() { return supplier; }
     public void setSupplier(String supplier) { this.supplier = supplier; }
+
+    public String getSupplierName() { return supplierName; }
+    public void setSupplierName(String supplierName) { this.supplierName = supplierName; }
 
     public String getPostingDate() { return postingDate; }
     public void setPostingDate(String postingDate) { this.postingDate = postingDate; }
@@ -23,4 +36,8 @@ public class FactureAchat {
 
     public double getGrandTotal() { return grandTotal; }
     public void setGrandTotal(double grandTotal) { this.grandTotal = grandTotal; }
+
+    public double getOutstandingAmount() { return outstandingAmount; }
+    public void setOutstandingAmount(double outstandingAmount) { this.outstandingAmount = outstandingAmount; }
 }
+
